53f3bc68cd11543b8b28d3c502616fcde58c18a9
shirasael <shira.asael@gmail.com>
2019-10-12 21:13:24 +0300

    added timer, changed name of project

---
+++ syncalong/src/client/timer.py
@@ -0,0 +1,24 @@
+import datetime
+import time
+
+from ntplib import NTPClient
+
+
+def wait_for_remote_time(remote_start_time: datetime.datetime,
+                         wait_time_seconds: int,
+                         ntp_server,
+                         ntp_port=123):
+    """
+    Wait the given amount of seconds, starting from the given remote time. The function returns when the remote
+    time is exactly remote_start_time + wait_time_seconds.
+    """
+    wait_delta = datetime.timedelta(seconds=wait_time_seconds)
+    target_time = remote_start_time + wait_delta
+    current_remote_time = get_remote_time(ntp_server, ntp_port)
+    if target_time > current_remote_time:
+        time.sleep((target_time - current_remote_time).seconds)
+
+
+def get_remote_time(ntp_server, ntp_port=123):
+    ntp_client = NTPClient()
+    return datetime.datetime.fromtimestamp(ntp_client.request(ntp_server, ntp_port).tx_time)
