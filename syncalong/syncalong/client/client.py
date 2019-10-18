69607f11eb937036fb056639c9cda7a6ebb9acc7
shirasael <shira.asael@gmail.com>
2019-10-18 12:04:13 +0300

    client docs

--- syncalong/syncalong/client/client.py
+++ syncalong/syncalong/client/client.py
@@ -20,8 +20,22 @@ class UnknownSignalException(Exception):
 
 
 class Client(object):
-
+    """
+    A client for synchronously playing music.
+    The client connects to a server at a given address, and can either play/stop music or receive files to be played.
+    All the clients received music files are saved in a local repository.
+    The client synchronizes with a remote NTP server, in order to guarantee music will be played at the same time for
+    all clients at once.
+    """
     def __init__(self, server_ip, server_port, ntp_server, music_files_repo):
+        """
+        Initialize a new client by connecting to the server at the given address.
+
+        :param server_ip: IP address of the server to connect to.
+        :param server_port: Port of the server to connect to.
+        :param ntp_server: Hostname of an NTP server to sync from.
+        :param music_files_repo: Local path of a directory which will be used as the client's repository.
+        """
         self.ntp_server = ntp_server
         self.music_files_repo = music_files_repo
         self.socket = LengthSocket(socket.AF_INET, socket.SOCK_STREAM)
@@ -29,6 +43,12 @@ class Client(object):
         self.media_player = None
 
     def start(self):
+        """
+        Send the first message to the server and establish a connection.
+        Then, accept new messages from the server.
+
+        All messages received are expected to be of type GeneralPacket.
+        """
         self.socket.send(bytes("hello", encoding="utf-8"))
         while True:
             try:
@@ -41,18 +61,31 @@ class Client(object):
                 print("An error occurred: {}".format(msg))
 
     def play(self, music_file_path):
+        """
+        Play the file at the given path. If the file is not found, an exception is thrown.
+        :param music_file_path: Local music file to be played.
+        """
         print("Playing {}".format(music_file_path))
         self.media_player = vlc.MediaPlayer(music_file_path)
         self.media_player.play()
 
     def stop(self):
+        """
+        Stop playing whatever is playing now. If nothing is played - a message is printed and no exception is thrown.
+        """
         if self.media_player is not None:
             print("Stopping music")
             self.media_player.stop()
         else:
             print("No music currently playing!")
 
-    def _handle_signal(self, signal_packet):
+    def _handle_signal(self, signal_packet: SignalPacket):
+        """
+        Handle a packet of type SignalPacket: start / stop playing music, after waiting for the required amount of
+        seconds (aligned with the NTP server's time).
+
+        :param signal_packet: packet to be handled.
+        """
         print("Got signal {}".format(signal_packet.signal))
         server_send_time = datetime.datetime.fromtimestamp(signal_packet.send_timestamp)
         delay = signal_packet.wait_seconds
@@ -66,7 +99,17 @@ class Client(object):
         else:
             raise UnknownSignalException(signal_packet.signal)
 
-    def _handle_file_sync(self, file_sync_packet):
+    def _handle_file_sync(self, file_sync_packet: FileSyncPacket):
+        """
+        Handle a packet of type FileSyncPacket: check if a required music file exists in the repository, or accept a
+        file being sent.
+        A file is considered as existing in the client if:
+            - A file with the name in the FileSyncPacket exists in the client's repository
+            - The file's length is the same as the length specified in the FileSyncPacket
+        When a file is received by the client, it overrides the file with the same name in the repository, if it exists.
+
+        :param file_sync_packet: packet to be handled.
+        """
         local_path = os.path.join(self.music_files_repo, file_sync_packet.file_name.decode('utf-8'))
         if file_sync_packet.message_type == WHO_HAS:
             print("Got who has!")
