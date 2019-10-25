b1341724ab51a0579c37cb96440b49b4c5ad7771
itaif <itaifain@gmail.com>
2019-10-25 13:54:31 +0300

    change number of clients - for testing

--- syncalong/syncalong/server.py
+++ syncalong/syncalong/server.py
@@ -2,12 +2,12 @@ import time
 from server.music_server import MusicServer
 
 if __name__ == "__main__":
-    music_file = r"C:\Users\Shira\Documents\20588\file_example_MP3_1MG.mp3"
+    music_file = r"D:\itai\pyhton\a.mp3"
     ms = MusicServer("0.0.0.0", 22222)
     ms.start()
     while True:
         try:
-            while len(ms.clients) == 0:
+            while len(ms.clients) <= 1:
                 continue
             ms.serve_music_file(music_file)
             ms.signal_play_all(music_file)
